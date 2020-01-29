62d9026b82d446b9a0b8685a7c28e3b7db0b4536
西本有希 <yukinko_glay@yahoo.co.jp>
2020-01-29 13:58:54 +0900

    tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,36 @@
 class TweetsController < ApplicationController
 
   def index
-    
+    # @tweet = Tweet.find(params[:id])
+    @tweets = Tweet.includes(:user).order("created_at DESC")
+  end
+
+  def show
+    # @tweet = Tweet.find(params[:id])
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
-    Tweet.create(text: "")
+    @tweet = Tweet.create(text: tweet_params[:text], user_id: current_user.id)
+    if @tweet.save
+      redirect_to :root
+    else
+      render 'new'
+    end
   end
+
+private
+  def tweet_params
+    params.permit(:text)
+  end
+
 end
 # Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
 
 # private
-# def tweets_params
-#   params.permit(text: "")
-# end
+#   def tweet_params
+#     params.permit(:image, :text)
+#   end
