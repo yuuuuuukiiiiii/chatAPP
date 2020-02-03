62ef4a2cbe68797c33113f2c3abe265fdeacd3d9
西本有希 <yukinko_glay@yahoo.co.jp>
2020-02-03 14:23:14 +0900

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
   resources :tweets do
+    resources :comments, only: [:index, :create]
+    # resources :comments
   # resource :tweets, only: [:new, :create, :show]
   end
 end
